71009d59b50cc73fd8c42ef5f349e42b006a5b63
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-08-19 20:52:32 +0600

    After appling coupon if increment or decrement cart total price will update

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -133,6 +133,19 @@ class CartController extends Controller
         $row = Cart::get($rowId);
         Cart::update($rowId, $row->qty - 1);
 
+        //after appling coupon subtotal and grand total will update
+         if(Session::has('coupon')){
+            $coupon_name = Session::get('coupon')['coupon_name'];
+            $coupon = Coupon::where('coupon_name',$coupon_name)->first();
+           
+           Session::put('coupon',[
+                'coupon_name' => $coupon->coupon_name, 
+                'coupon_discount' => $coupon->coupon_discount, 
+                'discount_amount' => round(Cart::total() * $coupon->coupon_discount/100), 
+                'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount/100 )
+            ]); 
+        }
+
         return response()->json('Decrement');
     }//end
 
@@ -140,6 +153,19 @@ class CartController extends Controller
     public function IncrementCart($rowId) {
         $row = Cart::get($rowId);
         Cart::update($rowId, $row->qty + 1);
+
+        //after appling coupon subtotal and grand total will update
+         if(Session::has('coupon')){
+            $coupon_name = Session::get('coupon')['coupon_name'];
+            $coupon = Coupon::where('coupon_name',$coupon_name)->first();
+           
+           Session::put('coupon',[
+                'coupon_name' => $coupon->coupon_name, 
+                'coupon_discount' => $coupon->coupon_discount, 
+                'discount_amount' => round(Cart::total() * $coupon->coupon_discount/100), 
+                'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount/100 )
+            ]); 
+        }
 
         return response()->json('Increment');
     }//end
