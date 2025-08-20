d62935e6762b8879eddd7de885c6a92a9dd9971b
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-08-20 15:33:34 +0600

    Cash on delivary page templete setup

--- resources/views/frontend/payment/cash.blade.php
+++ resources/views/frontend/payment/cash.blade.php
@@ -1,18 +1,19 @@
 @extends('frontend.master_dashboard')
 @section('main')
 
-<div class="page-header breadcrumb-wrap">
+
+ <div class="page-header breadcrumb-wrap">
             <div class="container">
                 <div class="breadcrumb">
                     <a href="index.html" rel="nofollow"><i class="fi-rs-home mr-5"></i>Home</a> 
-                    <span></span> Cash On Delivery Payment
+                    <span></span> Cash On Delivary
                 </div>
             </div>
         </div>
         <div class="container mb-80 mt-50">
             <div class="row">
                 <div class="col-lg-8 mb-40">
-                    <h3 class="heading-2 mb-10"> Cash On Delivery</h3>
+                    <h3 class="heading-2 mb-10">Cash On Delivary Payment</h3>
                     <div class="d-flex justify-content-between">
                        
                     </div>
@@ -24,7 +25,7 @@
 
                 	<div class="border p-40 cart-totals ml-30 mb-50">
     <div class="d-flex align-items-end justify-content-between mb-30">
-        <h4>Your Order</h4>
+        <h4>Your Order Details</h4>
        
     </div>
     <div class="divider-2 mb-30"></div>
@@ -32,12 +33,15 @@
 
  <table class="table no-border">
         <tbody>
-            <tr>
+
+        @if(Session::has('coupon'))
+
+         <tr>
                 <td class="cart_total_label">
                     <h6 class="text-muted">Subtotal</h6>
                 </td>
                 <td class="cart_total_amount">
-                    <h4 class="text-brand text-end">$12.31</h4>
+                    <h4 class="text-brand text-end">${{ $cartTotal }}</h4>
                 </td>
             </tr>
             
@@ -46,7 +50,7 @@
                     <h6 class="text-muted">Coupn Name</h6>
                 </td>
                 <td class="cart_total_amount">
-                    <h6 class="text-brand text-end">EASYLEA</h6>
+                    <h6 class="text-brand text-end">{{ session()->get('coupon')['coupon_name'] }} ( {{ session()->get('coupon')['coupon_discount'] }}% )</h6>
                 </td>
             </tr>
 
@@ -55,7 +59,7 @@
                     <h6 class="text-muted">Coupon Discount</h6>
                 </td>
                 <td class="cart_total_amount">
-                    <h4 class="text-brand text-end">$12.31</h4>
+                    <h4 class="text-brand text-end">${{ session()->get('coupon')['discount_amount'] }}</h4>
                 </td>
             </tr>
 
@@ -64,9 +68,24 @@
                     <h6 class="text-muted">Grand Total</h6>
                 </td>
                 <td class="cart_total_amount">
-                    <h4 class="text-brand text-end">$12.31</h4>
+                    <h4 class="text-brand text-end">${{ session()->get('coupon')['total_amount'] }}</h4>
                 </td>
             </tr>
+
+        @else
+
+       
+
+            <tr>
+                <td class="cart_total_label">
+                    <h6 class="text-muted">Grand Total</h6>
+                </td>
+                <td class="cart_total_amount">
+                    <h4 class="text-brand text-end">${{ $cartTotal }}</h4>
+                </td>
+            </tr>
+     @endif
+             
         </tbody>
     </table>
 
@@ -84,7 +103,7 @@
 <div class="col-lg-6">
 <div class="border p-40 cart-totals ml-30 mb-50">
     <div class="d-flex align-items-end justify-content-between mb-30">
-        <h4>Make Cash Payment </h4>
+        <h4>Make Cash On Delivary Payment</h4>
          
     </div>
     <div class="divider-2 mb-30"></div>
@@ -92,57 +111,42 @@
          
 
  <table class="table no-border">
-        <tbody>
-            <tr>
-                <td class="cart_total_label">
-                    <h6 class="text-muted">Subtotal</h6>
-                </td>
-                <td class="cart_total_amount">
-                    <h4 class="text-brand text-end">$12.31</h4>
-                </td>
-            </tr>
-            
-            <tr>
-                <td class="cart_total_label">
-                    <h6 class="text-muted">Coupn Name</h6>
-                </td>
-                <td class="cart_total_amount">
-                    <h6 class="text-brand text-end">EASYLEA</h6>
-                </td>
-            </tr>
+  <tbody>
+          <form action="{{ route('stripe.order') }}" method="post">
+        @csrf
 
-              <tr>
-                <td class="cart_total_label">
-                    <h6 class="text-muted">Coupon Discount</h6>
-                </td>
-                <td class="cart_total_amount">
-                    <h4 class="text-brand text-end">$12.31</h4>
-                </td>
-            </tr>
+        
+            <input type="hidden" name="name" value="{{ $data['shipping_name'] }}">
+            <input type="hidden" name="email" value="{{ $data['shipping_email'] }}">
+            <input type="hidden" name="phone" value="{{ $data['shipping_phone'] }}">
+            <input type="hidden" name="post_code" value="{{ $data['post_code'] }}">
+            <input type="hidden" name="division_id" value="{{ $data['division_id'] }}">
+            <input type="hidden" name="district_id" value="{{ $data['district_id'] }}">
+            <input type="hidden" name="state_id" value="{{ $data['state_id'] }}">
+            <input type="hidden" name="address" value="{{ $data['shipping_address'] }}">
+            <input type="hidden" name="notes" value="{{ $data['notes'] }}">
 
-              <tr>
-                <td class="cart_total_label">
-                    <h6 class="text-muted">Grand Total</h6>
-                </td>
-                <td class="cart_total_amount">
-                    <h4 class="text-brand text-end">$12.31</h4>
-                </td>
-            </tr>
-        </tbody>
+
+
+
+    <div class="form-row">
+        
+    </div>
+    <br>
+    <button class="btn btn-primary">Submit Payment</button>
+          </tbody>
     </table>
 
-
+</form>
 
 
 
     </div>
 </div>
-                     
-
-
-                </div>
-            </div>
-        </div>
+    
+     
+                </div> <!-- // end lg md 6 -->
+                    
 
 
 @endsection
