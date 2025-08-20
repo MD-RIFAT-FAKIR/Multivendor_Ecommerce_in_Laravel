84d446abb19604edb88b807af3e40de194f07660
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-08-20 14:17:46 +0600

    When submit stripe payment then order related all details will store in order table of database

--- app/Http/Controllers/User/StripeController.php
+++ app/Http/Controllers/User/StripeController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Order;
+use App\Models\OrderItem;
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Support\Facades\Session;
+use Carbon\Carbon;
+use Auth;
 use Stripe\Stripe;
 use Stripe\Charge;
 
@@ -17,15 +23,49 @@ class StripeController extends Controller
 
         $token = $request->stripeToken;
 
+        if(Session::has('coupon')){
+          $total_amount = Session::get('coupon')['total_amount'];
+        }else{
+          $total_amount = round(Cart::total());
+        }
+
         $charge = \Stripe\Charge::create([
-          'amount' => 999*100,
+          'amount' => $total_amount*100,
           'currency' => 'usd',
           'description' => 'Easy Mulit Vendor Shop',
           'source' => $token,
-          'metadata' => ['order_id' => '6735'],
+          'metadata' => ['order_id' => uniqid()],
         ]);
 
         dd($charge);
+
+        $order_id = Order::isertGetId([
+          'user_id' => Auth::id(),
+          'division_id'=> $request->division_id,
+          'district_id'=> $request->district_id,
+          'state_id'=> $request->state_id,
+          'name'=> $request->name,
+          'email'=> $request->email,
+          'phone'=> $request->phone,
+          'adress'=> $request->address,
+          'post_code'=> $request->post_code,
+          'notes'=> $request->notes,
+
+          'payment_type'=> $charge->payment_method,
+          'payment_method'=> 'Stripe',
+          'transaction_id'=> $charge->balance_transaction,
+          'currency'=> $charge->currency,
+          'amount'=> $total_amount,
+          'order_number'=> $charge->metadata->order_id,
+
+          'invoice_no'=> 'EOS'.mt_rand(10000000,99999999),
+          'order_date'=> Carbon::now()->format('d F Y'),
+          'order_month'=> Carbon::now()->format('F'),
+          'order_year'=> Carbon::now()->format('Y'),
+          'status'=> 'Pending',
+          'created_at' => Carbon::now(),
+        ]);
+        
 
     }// End Method
 }
