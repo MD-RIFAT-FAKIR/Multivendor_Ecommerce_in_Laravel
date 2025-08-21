cf2224bd342001717c66e5370b628835802a8b14
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-08-21 15:40:40 +0600

    Order details page setup of user dashboard

--- app/Http/Controllers/User/AllUserController.php
+++ app/Http/Controllers/User/AllUserController.php
@@ -37,7 +37,7 @@ class AllUserController extends Controller
     public function UserOrderDetails($order_id) {
         $order =  Order::with('division','district','state','user')->where('id', '=', $order_id)->where('user_id', '=', Auth::id())->first();
 
-        $orderItem = OrderItem::with('product')->where('order_id ', '=', $order_id)->orderBy('id','desc')->get();
+        $orderItem = OrderItem::with('product')->where('order_id', '=', $order_id)->orderBy('id','desc')->get();
 
         return view('frontend.order.order_details', compact('order','orderItem'));
     }
