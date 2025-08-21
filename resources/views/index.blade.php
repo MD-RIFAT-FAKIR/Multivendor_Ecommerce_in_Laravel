a1c3818b8cdf9a982cb4b9297b1e018d01b4d1e5
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-08-21 10:46:53 +0600

    Link dashboard route form acount details page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,7 +20,7 @@
                                 <div class="dashboard-menu">
                                     <ul class="nav flex-column" role="tablist">
                                         <li class="nav-item">
-                                            <a class="nav-link active" href="#dashboard"><i class="fi-rs-settings-sliders mr-10"></i>Dashboard</a>
+                                            <a class="nav-link active" href="{{ route('dashboard') }}"><i class="fi-rs-settings-sliders mr-10"></i>Dashboard</a>
                                         </li>
                                         <li class="nav-item">
                                             <a class="nav-link" href="#orders"><i class="fi-rs-shopping-bag mr-10"></i>Orders</a>
