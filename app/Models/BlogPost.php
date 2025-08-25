1a43b5eaa5fe4e24157654f14acbf774491c5c29
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-08-25 22:21:08 +0600

    Make relationship between BlogPost model and BlogCategory model inside BlogPost model to get BlogCategoty's table blog_category_name and display all blog post page

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -10,4 +10,7 @@ class BlogPost extends Model
     use HasFactory;
     protected $guarded = [];
 
+    public function blogcategory() {
+        return $this->belongsTo(BlogCategory::class, 'category_id');
+    }
 }
