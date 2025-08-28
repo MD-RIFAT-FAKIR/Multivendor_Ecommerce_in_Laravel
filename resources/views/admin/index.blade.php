10a91e103e03edb4e03d8aef2163d55a06df9c1c
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-08-28 15:56:03 +0600

    Display dynamic daily,monthly,yearly sale and total user,vendor,pending orders on admin dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,14 +1,33 @@
 @extends('admin.admin_dashboard')
 
 @section('admin')
-<div class="page-content">
 
+@php 
+	$date = date('d-m-y');
+	$todays_sale = App\Models\Order::where('order_date', $date)->sum('amount');
+
+	$month = date('F');
+	$monthly_sale = App\Models\Order::where('order_month', $month)->sum('amount');
+
+	$year = date('Y');
+	$yearly_sale = App\Models\Order::where('order_year', $year)->sum('amount');
+
+	$pendingOrders = App\Models\Order::where('status', 'pending')->get();
+
+	$totalUser = App\Models\User::where('role','user')->where('status','active')->get();
+	
+	$totalVendor = App\Models\User::where('role','vendor')->where('status','active')->get();
+
+@endphp
+
+
+<div class="page-content">
 <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
 	<div class="col">
 		<div class="card radius-10 bg-gradient-deepblue">
 			<div class="card-body">
 			<div class="d-flex align-items-center">
-				<h5 class="mb-0 text-white">9526</h5>
+				<h5 class="mb-0 text-white">${{ $todays_sale }} USD</h5>
 				<div class="ms-auto">
 					<i class='bx bx-cart fs-3 text-white'></i>
 				</div>
@@ -17,7 +36,7 @@
 				<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
 			</div>
 			<div class="d-flex align-items-center text-white">
-				<p class="mb-0">Total Orders</p>
+				<p class="mb-0">Today's Sale</p>
 				<p class="mb-0 ms-auto">+4.2%<span><i class='bx bx-up-arrow-alt'></i></span></p>
 			</div>
 		</div>
@@ -27,7 +46,7 @@
 		<div class="card radius-10 bg-gradient-orange">
 		<div class="card-body">
 			<div class="d-flex align-items-center">
-				<h5 class="mb-0 text-white">$8323</h5>
+				<h5 class="mb-0 text-white">${{ $monthly_sale }} USD</h5>
 				<div class="ms-auto">
 					<i class='bx bx-dollar fs-3 text-white'></i>
 				</div>
@@ -36,7 +55,7 @@
 				<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
 			</div>
 			<div class="d-flex align-items-center text-white">
-				<p class="mb-0">Total Revenue</p>
+				<p class="mb-0">Monthly Sale</p>
 				<p class="mb-0 ms-auto">+1.2%<span><i class='bx bx-up-arrow-alt'></i></span></p>
 			</div>
 		</div>
@@ -46,7 +65,7 @@
 		<div class="card radius-10 bg-gradient-ohhappiness">
 		<div class="card-body">
 			<div class="d-flex align-items-center">
-				<h5 class="mb-0 text-white">6200</h5>
+				<h5 class="mb-0 text-white">${{ $yearly_sale }} USD</h5>
 				<div class="ms-auto">
 					<i class='bx bx-group fs-3 text-white'></i>
 				</div>
@@ -55,7 +74,7 @@
 				<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
 			</div>
 			<div class="d-flex align-items-center text-white">
-				<p class="mb-0">Visitors</p>
+				<p class="mb-0">Yearly Sale</p>
 				<p class="mb-0 ms-auto">+5.2%<span><i class='bx bx-up-arrow-alt'></i></span></p>
 			</div>
 		</div>
@@ -65,7 +84,7 @@
 		<div class="card radius-10 bg-gradient-ibiza">
 			<div class="card-body">
 			<div class="d-flex align-items-center">
-				<h5 class="mb-0 text-white">5630</h5>
+				<h5 class="mb-0 text-white">{{ count($pendingOrders) }}</h5>
 				<div class="ms-auto">
 					<i class='bx bx-envelope fs-3 text-white'></i>
 				</div>
@@ -74,11 +93,49 @@
 				<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
 			</div>
 			<div class="d-flex align-items-center text-white">
-				<p class="mb-0">Messages</p>
+				<p class="mb-0">Pending Orders</p>
 				<p class="mb-0 ms-auto">+2.2%<span><i class='bx bx-up-arrow-alt'></i></span></p>
 			</div>
 		</div>
 		</div>
+	</div>
+	<div class="col">
+		<div class="card radius-10 bg-gradient-ibiza">
+			<div class="card-body">
+			<div class="d-flex align-items-center">
+				<h5 class="mb-0 text-white">{{ count($totalUser) }}</h5>
+				<div class="ms-auto">
+					<i class='bx bx-envelope fs-3 text-white'></i>
+				</div>
+			</div>
+			<div class="progress my-3 bg-light-transparent" style="height:3px;">
+				<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+			</div>
+			<div class="d-flex align-items-center text-white">
+				<p class="mb-0">Total User</p>
+				<p class="mb-0 ms-auto">+2.2%<span><i class='bx bx-up-arrow-alt'></i></span></p>
+			</div>
+		</div>
+		</div>
+	</div>
+	<div class="col">
+		<div class="card radius-10 bg-gradient-ohhappiness">
+		<div class="card-body">
+			<div class="d-flex align-items-center">
+				<h5 class="mb-0 text-white">{{ count($totalVendor) }}</h5>
+				<div class="ms-auto">
+					<i class='bx bx-group fs-3 text-white'></i>
+				</div>
+			</div>
+			<div class="progress my-3 bg-light-transparent" style="height:3px;">
+				<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+			</div>
+			<div class="d-flex align-items-center text-white">
+				<p class="mb-0">Total Vendor</p>
+				<p class="mb-0 ms-auto">+5.2%<span><i class='bx bx-up-arrow-alt'></i></span></p>
+			</div>
+		</div>
+	</div>
 	</div>
 </div><!--end row-->
 
